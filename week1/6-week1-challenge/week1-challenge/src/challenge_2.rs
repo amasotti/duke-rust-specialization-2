a687be0f7a74ea81891a9102d21fb1911c1dc1a4
Antonio Masotti <toniomasotti@gmail.com>
2024-06-01 19:41:48 +0200

    week1 - challenge2, refinement

--- week1/6-week1-challenge/week1-challenge/src/challenge_2.rs
+++ week1/6-week1-challenge/week1-challenge/src/challenge_2.rs
@@ -2,13 +2,16 @@ use std::collections::HashMap;
 use std::fs;
 use std::io::Error as IoError;
 
+const MIN_WORD_LENGTH: usize = 5;
+
 pub fn challenge_2() {
     println!("Challenge 2");
     let contents = get_file_content("data/poe_holmes.txt").expect("Could not read file");
 
     let words = get_words(&contents);
+    let cleaned_words = clean_up(words);
 
-    let words_freq = get_words_freq(words);
+    let words_freq = get_words_freq(cleaned_words);
 
     let top_10_words = take_n(words_freq, 10);
 
@@ -26,15 +29,57 @@ fn get_words(text: &str) -> Vec<&str> {
     text.split_whitespace().collect()
 }
 
-fn get_words_freq(words: Vec<&str>) -> HashMap<String, i32> {
+fn clean_up(words: Vec<&str>) -> Vec<String> {
+    let mut words_cleaned = Vec::new();
+
+    for word in words {
+        let mut w = word.to_string();
+
+        // Handle punctuation
+        w = w.chars().filter(|c| c.is_alphabetic()).collect();
+
+        // Convert to lowercase
+        w = w.to_lowercase();
+
+        // Handle empty strings
+        if w.is_empty() { continue; }
+
+        // Handle blacklist and minimum word length
+        if is_in_blacklist(&w) { continue; }
+
+        words_cleaned.push(w);
+    }
+
+    words_cleaned
+}
+
+fn get_words_freq(words: Vec<String>) -> HashMap<String, i32> {
     let mut words_freq = HashMap::new();
 
     for word in words {
-        let count = words_freq.entry(word.to_string()).or_insert(0);
+        let count = words_freq.entry(word).or_insert(0);
         *count += 1;
     }
 
     words_freq
+}
+
+fn is_in_blacklist(word: &str) -> bool {
+    let blacklist = [
+        "the", "and", "of", "to", "a", "in",
+        "that", "was", "he", "his", "it", "i",
+        "with", "as", "had", "for", "at", "by", "on", "not",
+        "be", "this", "but", "which", "from", "or", "have",
+        "an", "they", "one", "all", "we", "you", "were", "her",
+        "would", "there", "their", "will", "when", "who", "him",
+        "been", "more", "out", "up", "into", "if", "no",
+        "what", "so", "said", "its", "about", "them",
+        "then", "some", "only", "could", "she", "these",
+        "two", "may", "first", "any", "other", "do", "like",
+        "my", "over", "such"
+    ];
+
+    blacklist.contains(&word) || word.len() <= MIN_WORD_LENGTH
 }
 
 fn take_n(words_freq: HashMap<String, i32>, n: usize) -> HashMap<String, i32> {
