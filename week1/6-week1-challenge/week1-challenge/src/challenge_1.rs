b27667ca95374f0ce5bf3a07ebcb44125ac232ed
Antonio Masotti <toniomasotti@gmail.com>
2024-06-01 18:52:08 +0200

    week1 challenge 1- docs

--- week1/6-week1-challenge/week1-challenge/src/challenge_1.rs
+++ week1/6-week1-challenge/week1-challenge/src/challenge_1.rs
@@ -4,6 +4,14 @@ use std::error::Error;
 use std::fmt::Display;
 use std::fs::File;
 
+
+pub fn challenge_1() {
+    let data = get_data_from_csv("./data/sample-data.csv").unwrap();
+    let stats = calc_statistics(data);
+
+    println!("Data Statistics: {}", stats);
+}
+
 /// This struct represents a record in the CSV file
 /// The file itself is just a random one I googled
 #[derive(Debug, Deserialize)]
@@ -20,7 +28,7 @@ struct Record {
     units: String,
 }
 
-pub fn get_data_from_csv(path: &str) -> Result<Vec<f64>, Box<dyn Error>> {
+fn get_data_from_csv(path: &str) -> Result<Vec<f64>, Box<dyn Error>> {
     // Initialize CSV reader
     let mut reader = Reader::from_path(path).expect("Could not read file");
     // Initialize vector to store records
@@ -38,13 +46,22 @@ pub fn get_data_from_csv(path: &str) -> Result<Vec<f64>, Box<dyn Error>> {
     Ok(records)
 }
 
-pub struct Statistics {
+/// This struct holds the statistics of the data
+///
+/// It has the following fields:
+/// - min: The minimum value in the data
+/// - max: The maximum value in the data
+/// - mean: The mean of the data
+/// - variance: The variance of the data
+/// - std_dev: The standard deviation of the data
+struct Statistics {
     min: f64,
     max: f64,
     mean: f64,
     variance: f64,
     std_dev: f64,
 }
+
 
 impl Display for Statistics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -59,7 +76,7 @@ impl Display for Statistics {
     }
 }
 
-pub fn calc_statistics(data: Vec<f64>) -> Statistics {
+fn calc_statistics(data: Vec<f64>) -> Statistics {
     let n = data.len() as f64;
     let min = calc_min(&data);
     let max = calc_max(&data);
@@ -76,6 +93,16 @@ pub fn calc_statistics(data: Vec<f64>) -> Statistics {
     }
 }
 
+/// Calculate the minimum value in the data
+///
+/// ## How it is done in Rust
+///
+/// The `calc_min` function takes a reference to a vector of f64 values
+/// and returns the minimum value in the vector.
+///
+/// It uses the `iter` method to get an iterator over the vector,
+/// and the `min_by` method to get the minimum value.
+/// the `unwrap` method is used to get the value from the `Option` returned by `min_by`.
 fn calc_min(data: &Vec<f64>) -> f64 {
     *data
         .iter()
@@ -94,12 +121,32 @@ fn calc_mean(data: &Vec<f64>) -> f64 {
     data.iter().sum::<f64>() / data.len() as f64
 }
 
+/// Calculate the variance of the data
+///
+/// The variance of a set of data is the average of the squared differences of each value from the mean.
+/// or in formula form:
+///
+/// `variance = Σ(x - mean)^2 / n`
+///
+/// ## How it is done in Rust
+///
+/// The `calc_variance` function takes a reference to a vector of f64 values
+/// and returns the variance of the data.
+///
+/// It calculates the mean of the data using the `calc_mean` function,
+/// then uses the `map` method to get the squared difference of each value from the mean.
+/// The `sum` method is used to get the sum of the squared differences.
+/// Finally, the sum is divided by the length of the data to get the variance.
+///
+/// ## See
+/// [calc_mean](fn.calc_mean.html)
 fn calc_variance(data: &Vec<f64>) -> f64 {
     let n = data.len() as f64;
     let mean = calc_mean(data);
     data.iter().map(|x| (x - mean).powi(2)).sum::<f64>() / n
 }
 
+/// Calculate the standard deviation of the data
 fn calc_std_dev(data: &Vec<f64>) -> f64 {
     calc_variance(data).sqrt()
 }
